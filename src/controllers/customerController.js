96586598fe407b13a18356d9a587ed5af4e287da
Ritik <negiritik47@gmail.com>
2025-09-06 14:51:20 +0530

    customer controller

---
+++ src/controllers/customerController.js
@@ -0,0 +1,29 @@
+import { db } from "../config/db.js";
+import { orders } from "../models/order.js";
+import { products } from "../models/product.js";
+import { eq } from "drizzle-orm";
+import { OutOfStockException, InvalidProductException } from "../utils/errors.js";
+
+// Customer places an order
+export async function placeOrder(customerId, productId, quantity) {
+  // check product
+  const [product] = await db.select().from(products).where(eq(products.id, productId));
+
+  if (!product) throw new InvalidProductException("Product does not exist");
+
+  if (product.stock < quantity) throw new OutOfStockException("Not enough stock available");
+
+  // reduce stock safely
+  await db.update(products)
+    .set({ stock: product.stock - quantity })
+    .where(eq(products.id, productId));
+
+  // create order
+  const [order] = await db.insert(orders).values({
+    customerId,
+    productId,
+    quantity,
+  }).returning();
+
+  return order;
+}
